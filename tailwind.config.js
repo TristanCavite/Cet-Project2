8693c20a1374fddb9d61c49930d57c9eef8be5a6
DanBiron <19-1-01457@vsu.edu.ph>
2024-12-19 14:34:48 +0800

    force light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ const animate = require("tailwindcss-animate")
 const typography = require("@tailwindcss/typography");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  safelist: ["dark"],
+  darkMode: false,
   prefix: "",
   
   theme: {
