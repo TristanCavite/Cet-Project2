70de5500b8a5fe6633a877f54899828d2d036e61
DanBiron <19-1-01457@vsu.edu.ph>
2025-08-23 18:15:39 +0800

    public vieww/minor changes

--- pages/about/faculty.vue
+++ pages/about/faculty.vue
@@ -8,7 +8,7 @@
         class="object-cover w-full h-44 md:h-128"
       />
       <div class="absolute top-16 md:top-40 left-6 md:left-[120px] md:px-4 md:py-4 bg-gray-700/90 px-2 py-2">
-        <span class="text-xl text-white md:text-6xl">The College</span>
+        <span class="text-xl text-white md:text-6xl">The Faculty</span>
       </div>
     </div>
 
