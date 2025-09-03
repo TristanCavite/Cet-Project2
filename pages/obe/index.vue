9f03bc46d4d81a1a5b7fe5bb85d80446118cbf01
DanBiron <19-1-01457@vsu.edu.ph>
2025-09-03 22:39:53 +0800

    public view/ new tab bar and dynamic routing in mobile

---
+++ pages/obe/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <h1>Welcome to the OBE Page</h1>
+        <p>This is the content of the OBE page.</p>
+    </main>
+
+</template>
+
+<script setup lang="ts">
+definePageMeta({ layout: 'custom' })
+</script>
